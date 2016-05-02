a75b6d3464baef90ac5cbac04e1b65daa201a824
MGRiv <mgrivera501@gmail.com>
2016-05-02 14:04:22 -0400

    classwork

--- script.py
+++ script.py
@@ -24,3 +24,30 @@ def run(filename):
         
     for command in commands:
         print command
+        if command[0] == "push":
+            ptmp = new_matrix()
+            for c in range len(stack[len(stack) - 1]):
+                for r in range len(stack[len(stack) - 1][c]):
+                    ptmp[c][r] = stack[len(stack) - 1][c][r]
+            stack.append(ptmp)
+        elif command[0] == "pop":
+            stack.pop()
+        elif command[0] == "move":
+            stmp = make_translate(command[1],command[2],command[3])
+            matrix_mult(stmp,stack[len(stack) - 1])
+        elif command[0] == "rotate":
+            if command[1] == "x":
+                stmp = make_rotX(command[2])
+                matrix_mult(stmp,stack[len(stack) - 1])
+            elif command[1] == "y":
+                stmp = make_rotY(command[2])
+                matrix_mult(stmp,stack[len(stack) - 1])
+            else:
+                stmp = make_rotZ(command[2])
+                matrix_mult(stmp,stack[len(stack) - 1])
+        elif command[0] == "box":
+            add_box(screen,command[1],command[2],command[3],command[4],command[5],command[6])
+        elif command[0] == "sphere":
+            add_sphere(screen,command[1],command[2],command[3],command[4])
+        elif command[0] == "torus":
+            add_torus(screen,
